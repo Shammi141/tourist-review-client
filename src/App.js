c533d66ffe899b60d1903a7461686bd48d722230
Shammi141 <jannatulshammi181@gmail.com>
2022-11-08 01:52:43 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Router/Routes/Routes';
 
 function App() {
+
   return (
-    <div className="App">
-      <h2>Started</h2>
+    <div className="max-w-screen-lg mx-auto">
+      <RouterProvider router = {router}></RouterProvider>
     </div>
   );
 }
