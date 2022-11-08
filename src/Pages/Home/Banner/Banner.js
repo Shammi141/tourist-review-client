ccfaba2a8f0a92535009b512a72c942db109d71f
Shammi141 <jannatulshammi181@gmail.com>
2022-11-09 00:37:17 +0600

    made data & added offer section

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import img1 from '../../../assests/slider/s1.jpeg';
 import img2 from '../../../assests/slider/s2.jpeg';
-import img3 from '../../../assests/slider/s3.jpeg';
+import img3 from '../../../assests/slider/s4.jpg';
+import './Banner.css';
 
 const Banner = () => {
     return (
         <div className="carousel w-full">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src={img1} alt="" className="w-full h-2/3" />
+                <img src={img1} alt="" className="w-full h-2/3 carousel-img" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-20 right-5 top-1/4">
                     <h1 className='text-5xl font-bold text-white'>
                     Take A Tour <br />
@@ -27,6 +28,18 @@ const Banner = () => {
             </div> 
             <div id="slide2" className="carousel-item relative w-full">
                 <img src={img2} alt="" className="w-full h-2/3" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-20 right-5 top-1/4">
+                    <h1 className='text-5xl font-bold text-white'>
+                    Take A Tour <br />
+                    Give You Time <br /> 
+                    To Explore You <br />
+                    </h1>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-20 right-5 top-1/2 w-2/5">
+                    <p className='text-xl text-white '>
+                        Tour makes you happy and Gives you energy to step forward. 
+                    </p>
+                </div>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a> 
                     <a href="#slide3" className="btn btn-circle">❯</a>
@@ -34,6 +47,18 @@ const Banner = () => {
             </div> 
             <div id="slide3" className="carousel-item relative w-full">
                 <img src={img3} alt="" className="w-full h-2/3" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-20 right-5 top-1/4">
+                    <h1 className='text-5xl font-bold text-white'>
+                    Take A Tour <br />
+                    Give You Time <br /> 
+                    To Explore You <br />
+                    </h1>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-20 right-5 top-1/2 w-2/5">
+                    <p className='text-xl text-white '>
+                        Tour makes you happy and Gives you energy to step forward. 
+                    </p>
+                </div>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a> 
                     <a href="#slide4" className="btn btn-circle">❯</a>
---
+++ src/Pages/Home/OfferCard/OfferCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import offer from '../../../assests/offer/offer1.jpeg';
+
+const OfferCard = () => {
+    return (
+        <div className="card m-8 p-6 w-100% bg-base-100 shadow-xl">
+            {/* <figure  className='w-100%'></figure> */}
+            <img className='max-h-96 p-20 rounded-md' src={offer} alt="Shoes" />
+            <div className="card-body">
+                <h2 className="card-title text-blue-600">
+                Wanna Get Best Offer?
+                <div className="badge badge-secondary ">New Offer</div>
+                </h2>
+                <p>Discover new place with your ones who is the best and nearest. <br />
+                Hey! We are giving the best offer to you. Come to grab & explore the world.
+                </p>
+                <div className="card-actions justify-end">
+                <div className="badge text-blue-600 badge-outline">General</div> 
+                <div className="badge text-blue-600 badge-outline">Premium</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OfferCard;
